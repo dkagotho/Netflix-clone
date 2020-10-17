97a0112d2e1db4be748117f9d41f1fe543f2122b
Diana kagotho <dianakago2@gmail.com>
2020-10-17 19:10:39 -0400

    fixed clone background

---
+++ src/Banner.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  return (
+    <header className="banner">
+      style=
+      {{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+    "http://image.tmdb.org/t/original/${movie?.backdrop_path}"
+)`,
+        backgroundPosition: "center center",
+      }}
+      <div className="banner__contents">
+        {/* title */}
+        <h1></h1>
+        {/* div > 2 buttons */}
+        {/* description */}
+      </div>
+    </header>
+  );
+}
+
+export default Banner;
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import "./Row.css";
 
 const base_Url = "http://images.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -25,8 +25,10 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${base_Url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_Url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
