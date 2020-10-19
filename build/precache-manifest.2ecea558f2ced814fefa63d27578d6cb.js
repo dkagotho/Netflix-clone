1e44a5b131bf01693cdbfddbf7ee581778e58614
Diana kagotho <dianakago2@gmail.com>
2020-10-19 12:15:59 -0400

    deployed using firebase

---
+++ build/precache-manifest.2ecea558f2ced814fefa63d27578d6cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71b31072299656d02c72e32bff6aeeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da7833474f6d49ed2d63",
+    "url": "/static/css/main.763151de.chunk.css"
+  },
+  {
+    "revision": "e06cdfe15ec9e8cc53da",
+    "url": "/static/js/2.ab23a3f3.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/static/js/2.ab23a3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7833474f6d49ed2d63",
+    "url": "/static/js/main.4303ce4f.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
